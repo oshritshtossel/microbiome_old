ca37d3f62c291008acbc74926a15b3aea1cdf853
arielrz02 <ariel.r.zvi@gmail.com>
2021-06-07 11:48:27 +0300

    rolling back to discarding according to final corrs

--- LearningMethods/textreeCreate.py
+++ LearningMethods/textreeCreate.py
@@ -12,7 +12,7 @@ every bacteria is an object to easily store it's information
 class Bacteria:
     def __init__(self, string, val):
         string = string.replace(" ", "")
-        lst = re.split(";|__", string)
+        lst = re.split("; |__", string)
         self.val = val
         # removing letters and blank spaces
         for i in range(0, len(lst)):
@@ -22,7 +22,7 @@ class Bacteria:
         self.lst = lst
 
 
-def create_tax_tree(series, remove_flag=None):
+def create_tax_tree(series, flag=None):
     tempGraph = nx.Graph()
     """workbook = load_workbook(filename="random_Otus.xlsx")
     sheet = workbook.active"""
@@ -35,27 +35,27 @@ def create_tax_tree(series, remove_flag=None):
         tempGraph.add_edge("anaerobe", (bac[i].lst[0],))
         # connecting all levels of the taxonomy
         for j in range(0, len(bac[i].lst) - 1):
-            updateval(tempGraph, bac[i], valdict, j, True, remove_flag)
+            updateval(tempGraph, bac[i], valdict, j, True)
         # adding the value of the last node in the chain
-        updateval(tempGraph, bac[i], valdict, len(bac[i].lst) - 1, False, remove_flag)
+        updateval(tempGraph, bac[i], valdict, len(bac[i].lst) - 1, False)
     valdict["anaerobe"] = valdict[("Bacteria",)] + valdict[("Archaea",)]
-    return create_final_graph(tempGraph, valdict)
+    return create_final_graph(tempGraph, valdict, flag)
 
 
-def updateval(graph, bac, vald, num, adde, flag):
+def updateval(graph, bac, vald, num, adde):
     if adde:
         graph.add_edge(tuple(bac.lst[:num+1]), tuple(bac.lst[:num+2]))
     # adding the value of the nodes
-    if bac.val != flag:
-        if tuple(bac.lst[:num+1]) in vald:
-            vald[tuple(bac.lst[:num+1])] += bac.val
-        else:
-            vald[tuple(bac.lst[:num+1])] = bac.val
+    if tuple(bac.lst[:num+1]) in vald:
+        vald[tuple(bac.lst[:num+1])] += bac.val
+    else:
+        vald[tuple(bac.lst[:num+1])] = bac.val
 
 
-def create_final_graph(tempGraph, valdict):
+def create_final_graph(tempGraph, valdict, flag):
     graph = nx.Graph()
     for e in tempGraph.edges():
+        if flag is None or valdict[e[0]] * valdict[e[1]] != 0:
             graph.add_edge((e[0], valdict[e[0]]),
                            (e[1], valdict[e[1]]))
     return graph
